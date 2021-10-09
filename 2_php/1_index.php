9f803604dc8a843f5143d901700ecfb51efd8f84
NikitaYefimov <92211491+NikitaYefimov@users.noreply.github.com>
2021-10-09 14:50:37 +0200

    Add files via upload

---
+++ 2_php/1_index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title> Strona głowna </title>
+  </head>
+  <body>
+    <p>Strona głowna</p>
+  </body>
+</html>
